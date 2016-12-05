06b1e57ea813f2ea2ccfd8c17f19d7fa07dca986
Rostislav-Bokoch <bokoch-98@bk.ru>
2016-12-05 20:24:09 +0200

    done

---
+++ Help.cpp
@@ -0,0 +1,7 @@
+#include <CMath>
+#include "CEquation.h"
+#include "Help.h"
+
+double Equation (CEquation &equation) {
+    return 5 * atan(equation.x) - 1/4 * acos(equation.x) * ((equation.x + 3 * fabs(equation.x - equation.y)+equation.x*equation.x) / (fabs(equation.x-equation.y) * equation.z + equation.x * equation.x));
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <Cmath>
+#include "CEquation.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    CEquation *equation = new CEquation (0.1722, 6.33, 3.25 * pow(10, -4));
+
+    cout << Equation (*equation) << endl;
     return 0;
 }
