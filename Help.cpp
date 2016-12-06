d4ebf1b33da798253828519a203e5bb77dc9dbe1
Rostislav-Bokoch <bokoch-98@bk.ru>
2016-12-06 13:53:26 +0200

    Fix formula

--- Help.cpp
+++ Help.cpp
@@ -3,5 +3,5 @@
 #include "Help.h"
 
 double Equation (CEquation &equation) {
-    return 5 * atan(equation.x) - 1/4 * acos(equation.x) * ((equation.x + 3 * fabs(equation.x - equation.y)+equation.x*equation.x) / (fabs(equation.x-equation.y) * equation.z + equation.x * equation.x));
+    return 5.0 * atan(equation.x) - 1.0/4.0 * acos(equation.x) * ((equation.x + 3.0 * fabs(equation.x - equation.y)+equation.x*equation.x) / (fabs(equation.x-equation.y) * equation.z + equation.x * equation.x));
 }
